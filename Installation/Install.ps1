5868e659fcbadff14eb9a834d34da84e3df8e85e
Peter Kjaer <pkjaer.sdl@gmail.com>
2015-12-10 12:12:52 +0100

    Included the Web 8 client in the installation script.

--- Installation/Install.ps1
+++ Installation/Install.ps1
@@ -12,6 +12,7 @@ $files = @(
 	'Clients/Tridion.ContentManager.CoreService.Client.2011sp1.dll', 
 	'Clients/Tridion.ContentManager.CoreService.Client.2013.dll', 
 	'Clients/Tridion.ContentManager.CoreService.Client.2013sp1.dll',
+	'Clients/Tridion.ContentManager.CoreService.Client.Web_8_1.dll',
 	'Installation/Install.ps1',
 	'Installation/Verify.ps1',
 	'Client.psm1', 
